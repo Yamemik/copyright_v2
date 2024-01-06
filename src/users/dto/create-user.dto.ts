8d28f3d61370ad012dbba3cb0d810d24ba1177cd
Yamemik <kuancarlos@mail.ri>
2024-01-06 23:06:02 +0500

    +nu,ber access

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { UserAccessLevel } from "../entities/user.entity";
 
 
 export class CreateUserDto {
     @ApiProperty({ default: 'mail@mail.ru' })
     mail: string;
-    @ApiProperty({
-        type: "enum",
-        enum: UserAccessLevel,
-        default: UserAccessLevel.LVL0,
-    })
-    access_level: UserAccessLevel;
+    @ApiProperty({ default: 0 })
+    access_level: Number;
 }
