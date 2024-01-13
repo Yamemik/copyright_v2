f5b45983465697844d5924f95afabe1fa8e9b27b
Yamemik <kuancarlos@mail.ri>
2024-01-13 11:58:19 +0500

    fix settings

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -7,29 +7,29 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('api/settings')
 @ApiTags('settings')
 export class SettingsController {
-  constructor(private readonly settingsService: SettingsService) { }
+	constructor(private readonly settingsService: SettingsService) { }
 
-  // @Post()
-  // @ApiOperation({ summary: 'Создать настройки (выполнить 1 раз)' })
-  // async create(@Body() createSettingDto: CreateSettingDto) {
-  //   return this.settingsService.create(createSettingDto);
-  // }
+	// @Post()
+	// @ApiOperation({ summary: 'Создать настройки (выполнить 1 раз)' })
+	// async create(@Body() createSettingDto: CreateSettingDto) {
+	// 	return this.settingsService.create(createSettingDto);
+	// }
 
-  @Get()
-  @ApiOperation({ summary: 'Получить настройки' })
-  async findOne() {
-    return this.settingsService.findOne();
-  }
+	@Get()
+	@ApiOperation({ summary: 'Получить настройки' })
+	async findOne() {
+		return this.settingsService.findOne();
+	}
 
-  // @Get('all')
-  // @ApiOperation({ summary: 'Получить все настройки' })
-  // async find() {
-  //   return this.settingsService.find();
-  // }
+	// @Get('all')
+	// @ApiOperation({ summary: 'Получить все настройки' })
+	// async find() {
+	// 	return this.settingsService.find();
+	// }
 
-  @Patch()
-  @ApiOperation({ summary: 'Изменить настройки' })
-  async update(@Body() updateSettingDto: CreateSettingDto) {
-    return this.settingsService.update(updateSettingDto);
-  }
+	@Patch()
+	@ApiOperation({ summary: 'Изменить настройки' })
+	async update(@Body() updateSettingDto: CreateSettingDto) {
+		return this.settingsService.update(updateSettingDto);
+	}
 }
--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -7,24 +7,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class SettingsService {
-  constructor(
-    @InjectRepository(SettingEntity)
-    private repository: Repository<SettingEntity>
-  ) { }
+	constructor(
+		@InjectRepository(SettingEntity)
+		private repository: Repository<SettingEntity>
+	) { }
 
-  async create(createSettingDto: CreateSettingDto) {
-    return await this.repository.save(createSettingDto);
-  }
+	async create(createSettingDto: CreateSettingDto) {
+		return await this.repository.save(createSettingDto);
+	}
 
-  async findOne() {
-    return await this.repository.findOneByOrFail({ id: 2 });
-  }
+	async findOne() {
+		return await this.repository.findOneByOrFail({ id: 1 });
+	}
 
-  async find() {
-    return await this.repository.find({});
-  }
+	async find() {
+		return await this.repository.find({});
+	}
 
-  async update(updateSettingDto: CreateSettingDto) {
-    return await this.repository.update({ id: 2 }, updateSettingDto);
-  }
+	async update(updateSettingDto: CreateSettingDto) {
+		return await this.repository.update({ id: 1 }, updateSettingDto);
+	}
 }
